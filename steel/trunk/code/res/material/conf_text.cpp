61daa5f9bdba3257ad182e31a7d1b535f322f5ef
kindex <kindex@99e027b2-380c-0410-a737-a5784e1a7bde>
2006-03-26 15:47:41 +0000

    made a copy
    git-svn-id: svn://localhost/steel.new/trunk@54 99e027b2-380c-0410-a737-a5784e1a7bde

---
+++ steel/trunk/code/res/material/conf_text.cpp
@@ -0,0 +1,34 @@
+#include "material_conf.h"
+#include "../../common/logger.h"
+#include <fstream>
+
+bool MaterialConf::init(const std::string name, ResCollection &res)
+{
+	std::string file = "../res/material/" + name + ".mat";
+
+	rstream f(file, 0); // no binary acces, only text
+	if(!f.good()) 
+	{
+		alog.out("Res/Material/Conf: cannot open file %s", file.c_str());
+		return false;
+	}
+
+	while(f.good())
+	{
+		char buf[1024];
+		f.getline(buf, 1023);
+		std::string s = buf;
+
+		if(s.substr(0,2) == "//" || s[0] == ';' || s[0] == '#') continue;
+
+		int d = s.find('=');
+		if(d<0) continue;
+		std::string key = s.substr(0, d);
+		std::string value = s.substr(d+1, s.size()-d-1);
+
+		var_s[key] = value;
+	//	var_f->insert(key, atof(value.c_str()));
+	}
+
+	return true;
+}
