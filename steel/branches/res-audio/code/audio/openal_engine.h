5fd09dd7863b079535f1715e68c0f7d3c595ca06
kuzmi4 <kuzmi4@99e027b2-380c-0410-a737-a5784e1a7bde>
2006-07-28 11:24:42 +0000

    git-svn-id: svn://localhost/steel.new/trunk@177 99e027b2-380c-0410-a737-a5784e1a7bde

---
+++ steel/branches/res-audio/code/audio/openal_engine.h
@@ -0,0 +1,13 @@
+#ifndef __OPENAL_ENGINE_H
+#define __OPENAL_ENGINE_H
+
+#include "openal/al.h"
+#include "openal/alc.h"
+#include "openal/alu.h"
+#include "openal/alut.h"
+
+//#pragma comment(lib, "openal32.lib")
+//#pragma comment(lib, "alut.lib")
+
+
+#endif
