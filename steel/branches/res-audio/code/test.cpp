159b9a9df554277a04f47c04f9daaeea7edf743c
kuzmi4 <kuzmi4@99e027b2-380c-0410-a737-a5784e1a7bde>
2006-07-29 16:31:25 +0000

    git-svn-id: svn://localhost/steel.new/trunk@182 99e027b2-380c-0410-a737-a5784e1a7bde

--- steel/branches/res-audio/code/test.cpp
+++ steel/branches/res-audio/code/test.cpp
@@ -18,8 +18,19 @@
 #include "_cpp.h"
 #include "common/logger.h"
 
+#include "res/audio/wav.h"
+#include "audio/openal_engine.h"
+
 bool test()
 {
+	WAV *tSnd = new WAV;
+	if (!initializeOpenAL())
+		return false;
 
+	tSnd->load("test.wav");	
+
+	alSourcePlay(tSnd->itsSourceID);
+
+	//destroyOpenAL();
 	return true;
 }
