63f2faf3beff6dd818392b6271b602b4f45e13b4
kindex <kindex@99e027b2-380c-0410-a737-a5784e1a7bde>
2006-12-29 00:46:41 +0000

    +jpeg dummy loader
    git-svn-id: svn://localhost/steel.new/trunk@427 99e027b2-380c-0410-a737-a5784e1a7bde

---
+++ steel/trunk/sources/res/image/jpeg.cpp
@@ -0,0 +1,49 @@
+/*id*********************************************************
+	File: res/image/jpeg.h
+	Unit: res/image
+	Part of: Steel engine
+	(C) DiVision, 2004-2006
+	Authors:
+		* KindeX [Andrey Ivanov, kindex@kindex.lv, http://kindex.lv]
+	License:
+		Steel Engine License
+	Description:
+		JPEG loader (using NV_UTIL lib)
+ ************************************************************/
+
+#include "../../steel.h"
+#include "jpeg.h"
+#include "../../common/logger.h"
+#include "../rstream.h"
+#include <string> 
+
+Image* createJPEG(const std::string filename)
+{
+	ResJPEG *o = new ResJPEG;
+	if(o->init(filename))
+	{
+		o->setId(objectIdGenerator.genUid());
+		return o;
+	}
+	else
+	{
+		delete o;
+		return NULL;
+	}
+}
+
+
+
+bool ResJPEG::init(const std::string name)
+{  
+	rstream f;
+
+	if(!f.open(name, "jpeg"))
+	{
+//		log_msg("warning res image tga", string("Res/Image/TGA: cannot open file ")+ name.c_str());
+		return false;
+	}
+
+	return true;
+}
+
---
+++ steel/trunk/sources/res/image/jpeg.h
@@ -0,0 +1,29 @@
+/*id*********************************************************
+	File: res/image/jpeg.h
+	Unit: res/image
+	Part of: Steel engine
+	(C) DiVision, 2004-2006
+	Authors:
+		* KindeX [Andrey Ivanov, kindex@kindex.lv, http://kindex.lv]
+	License:
+		Steel Engine License
+	Description:
+		JPEG loader (using NV_UTIL lib)
+ ************************************************************/
+
+#ifndef __RES_IMAGE_JPEG_H
+#define __RES_IMAGE_JPEG_H
+
+#include "../../steel.h"
+#include "image.h"
+
+class ResJPEG: public Image
+{
+public:
+	bool init(const std::string name);
+};
+
+Image* createJPEG(const std::string filename);
+
+
+#endif
