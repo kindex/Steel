526c5e58f21f25a4d81cd351975496f8a2edcaff
kindex <kindex@99e027b2-380c-0410-a737-a5784e1a7bde>
2007-01-01 16:36:04 +0000

    execute command (script setup) [fix]
    git-svn-id: svn://localhost/steel.new/trunk@437 99e027b2-380c-0410-a737-a5784e1a7bde

---
+++ steel/trunk/sources/res/config/config_setup.cpp
@@ -0,0 +1,60 @@
+/*id*********************************************************
+	File: res/config/config.h
+	Unit: res config
+	Part of: Steel engine
+	(C) DiVision, 2006
+	Authors:
+		* KindeX [Andrey Ivanov, kindex@kindex.lv, http://kindex.lv]
+	License:
+		Steel Engine License
+	Description:
+		Класс для хранения настроек
+ ************************************************************/
+#include "../../steel.h"
+
+#include "config_setup.h"
+
+using namespace std;
+
+bool executeCommand(Config* conf, std::string command)
+{
+	if(command.empty()) return true;
+	log_msg("console", "ExecCommand: '" + command + "'");
+
+	// var=value
+
+	svector<string> token;
+	explode('=', command, token);
+	if(token.size() != 2) return false;
+
+	token[0] = trim(token[0]);
+	token[1] = trim(token[1]);
+	Config* oldValue = conf->find(token[0]);
+	if (oldValue == NULL)
+	{
+		log_msg("script", "'" + token[0] + "' not found");
+		return false;
+	}
+	else
+	{
+		oldValue->setValues(token[0], token[1]);
+		return true;
+	}
+}
+
+
+bool executeScript(Config* conf, std::string script)
+{
+	log_msg("console", "ExecScript: '" + script + "'");
+
+	svector<string> lines;
+	explode(';', script, lines);
+	for(svector<string>::const_iterator it = lines.begin(); it != lines.end(); it++)
+	{
+		if(!executeCommand(conf, *it))
+		{
+			return false;
+		}
+	}
+	return true;
+}
---
+++ steel/trunk/sources/res/config/config_setup.h
@@ -0,0 +1,23 @@
+/*id*********************************************************
+	File: res/config/config.h
+	Unit: res config
+	Part of: Steel engine
+	(C) DiVision, 2006
+	Authors:
+		* KindeX [Andrey Ivanov, kindex@kindex.lv, http://kindex.lv]
+	License:
+		Steel Engine License
+	Description:
+		Класс для хранения настроек
+ ************************************************************/
+#ifndef __RES__CONFIF_SETUP_H__
+#define __RES__CONFIF_SETUP_H__
+
+#include "../../steel.h"
+
+
+#include "config.h"
+
+bool executeScript(Config* conf, std::string script);
+
+#endif
